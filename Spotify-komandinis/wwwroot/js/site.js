ffbc3d91a99dc32a76759e4abf906d732950e522
Arūnas Zamalis <aruzam@ktu.lt>
2021-03-01 14:37:00 +0200

    Pridėtas log in mygtukas, peradresuoja į /overview su access key

--- Spotify-komandinis/wwwroot/js/site.js
+++ Spotify-komandinis/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var express = require('express'); // Express web server framework
+var app = express();
+app.use(express.static(__dirname + '/public'));
+console.log('Listening on 8888');
+app.listen(8888);

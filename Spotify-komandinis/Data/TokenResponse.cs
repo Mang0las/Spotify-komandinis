93ac87eac155e51b60405c6a014b4e7d7e2d47b1
Arūnas Zamalis <aruzam@ktu.lt>
2021-03-02 15:18:23 +0200

    pakeitimai

---
+++ Spotify-komandinis/Data/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spotify_komandinis.Data
+{
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+    }
+}
---
+++ Spotify-komandinis/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spotify_komandinis.Data
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}

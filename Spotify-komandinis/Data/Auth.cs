40affa4d2b81dd897eec01dde8e722ddf12d8875
Arūnas Zamalis <aruzam@ktu.lt>
2021-03-14 19:33:32 +0200

    Fixed trackList

--- Spotify-komandinis/Data/Auth.cs
+++ Spotify-komandinis/Data/Auth.cs
@@ -7,7 +7,7 @@ namespace Spotify_komandinis
 {
     public class Auth
     {
-        public string redirectURL = "https://localhost:5001/Callback";
+        public string redirectURL = "https://localhost:44382/Callback";
         public string clientID = "6223f17b58004867882985508d43150d";
         public string clientSecret = ""; // DO NOT PUSH SECRET TO GITHUB
 
